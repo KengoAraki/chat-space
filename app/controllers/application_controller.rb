2c868e0f17a1730ef41c8dcdcffa4693e1e9b023
Kengo Araki <KengoAraki@arakikengo-no-MacBook-Air.local>
2017-05-02 21:15:20 +0900

    remove unnecessary method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
-
-  def after_sign_in_path_for(resource)
-    root_path
-  end
 end
