3346fc3c78f6d5d59bef99cb66b0383827ffac93
Kengo Araki <KengoAraki@arakikengo-no-MacBook-Air.local>
2017-05-06 18:27:55 +0900

    edit groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,22 @@
 class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+    @users = User.all
+  end
+
+  def create
+    @group= Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      redirect_to new_group_path, alert: "グループ名を入力してください"
+    end
+  end
+
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
 end
