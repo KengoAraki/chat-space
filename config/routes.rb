7c51444a5e14c276b6974e2f78755b8bd1781100
Kengo Araki <KengoAraki@arakikengo-no-MacBook-Air.local>
2017-05-06 18:27:33 +0900

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index';
 
-  resources :groups, only: [:new, :edit] do
+  resources :groups, only: [:new, :create] do
     resources :messages, only: [:index]
   end
 end
