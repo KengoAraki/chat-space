188f4c0b176a1308955eb2521cf907db4ce9aa08
Kengo Araki <KengoAraki@arakikengo-no-MacBook-Air.local>
2017-05-14 14:01:47 +0900

    edit groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,14 +6,14 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group= Group.new(group_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: "グループを作成しました"
     else
-      redirect_to new_group_path, alert: "グループ名を入力してください"
+      flash.now[:alert] = "グループ名を入力してください"
+      render :new
     end
   end
-
 
   private
   def group_params
