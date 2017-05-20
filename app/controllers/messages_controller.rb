340519ed87cb4352d920107bee1ab9a70cbb9033
Kengo Araki <KengoAraki@arakikengo-no-MacBook-Air.local>
2017-05-20 11:22:12 +0900

    edit messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,27 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :set_variables, only: [:index, :create]
 
   def index
-    @groups = current_user.groups
-    @group = Group.find(params[:group_id])
-    @messages = @group.messages
     @message = Message.new
   end
 
   def create
     @message = Message.new(message_params)
-    @groups = current_user.groups
-    @group = Group.find(params[:group_id])
-    @messages = @group.messages
     if @message.save
       redirect_to group_messages_path(@message.group_id), notice: "メッセージが投稿されました"
     else
       flash.now[:alert] = "メッセージが投稿されませんでした"
-      render action: :index
+      render :index
     end
   end
 
   private
+
+  def set_variables
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+  end
+
   def message_params
-    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.require(:message).permit(:body).merge(group_id: @group.id, user_id: current_user.id)
   end
 end
