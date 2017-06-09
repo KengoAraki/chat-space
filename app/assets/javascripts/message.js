8ba2c60cefe004739d6b994ced78b1846a2578ed
Kengo Araki <KengoAraki@arakikengo-no-MacBook-Air.local>
2017-06-09 20:56:38 +0900

    edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -12,6 +12,20 @@ $(function() {
     return html;
   };
 
+  function buildFlash_notice(notice){
+    var flash = `<div class="flash-alert-notice">
+                  ${ notice }
+                </div>`;
+    return flash;
+  }
+
+  function buildFlash_alert(alert){
+    var flash = `<div class="flash-alert-alert">
+                  ${ alert }
+                </div>`;
+    return flash;
+  }
+
   function scroll() {
     var height = $('.chat-main__message.clearfix#319').outerHeight();
     $('.chat-main__body').animate({scrollTop: height}, 300);
@@ -21,7 +35,8 @@ $(function() {
     e.preventDefault();
     var textField = $('#message_body');
     var message = textField.val();
-
+    $('.flash-alert-notice').remove();
+    $('.flash-alert-alert').remove();
     $.ajax({
       type: 'POST',
       url: './messages',
@@ -35,12 +50,17 @@ $(function() {
 
     .done(function(message) {
       var html = buildHTML(message);
+      var notice = "メッセージが投稿されました";
+      var flash = buildFlash_notice(notice);
+      $('body').prepend(flash);
       $('.chat-main__message.clearfix#319').append(html);
       $('#new_message')[0].reset();
       scroll();
     })
     .fail(function() {
-      alert("メッセージを入力してください");
+      var alert = "メッセージを入力してください";
+      var flash = buildFlash_alert(alert);
+      $('body').prepend(flash);
     });
     return false;
   });
