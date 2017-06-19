a5fbcebdce1206a5548d1bdb44f6c921e4bfc085
Kengo Araki <KengoAraki@arakikengo-no-MacBook-Air.local>
2017-06-19 15:20:15 +0900

    edit messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,10 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
@@ -13,7 +17,6 @@ class MessagesController < ApplicationController
         format.json
       end
     else
-      # flash.now[:alert] = "メッセージが投稿されませんでした"
       render :index
     end
   end
